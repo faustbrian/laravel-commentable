ec1c28ae5ebcf67a74b6fb111b27333617fa15d2
Brian Faust <hello@brianfaust.de>
2017-02-22 08:09:33 +0200

    Add Patreon note

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Laravel Commentable
 
+I would appreciate you taking the time to look at my [Patreon](https://www.patreon.com/faustbrian) and considering to support me if I'm saving you some time with my work.
+
 ## Installation
 
 Require this package, with [Composer](https://getcomposer.org/), in the root directory of your project.
--- src/HasComments.php
+++ src/HasComments.php
@@ -11,15 +11,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Commentable.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace BrianFaust\Commentable;
 
 use Illuminate\Database\Eloquent\Model;
