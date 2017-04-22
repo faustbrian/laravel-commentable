7c74c240b13185eb7ff9d3f6c1de177c6533cef7
Brian Faust <hello@brianfaust.de>
2017-04-22 08:45:06 +0300

    Try to fix the mess StyleCI makes

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -1,24 +1,10 @@
 <?php
 
-/*
- * This file is part of Laravel Commentable.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Commentable.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 namespace BrianFaust\Tests\Commentable;
 
