6eb27281f2c81532593de4aad916a1b31d3927c1
Brian Faust <faustbrian@users.noreply.github.com>
2017-08-02 12:06:18 +0300

    Apply fixes from StyleCI (#34)
    [ci skip] [skip ci]

--- src/CommentableServiceProvider.php
+++ src/CommentableServiceProvider.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Commentable.
  *
- * (c) Brian Faust <hello@brianfaust.me>
+ * (c) Brian Faust <hello@brianfaust.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
